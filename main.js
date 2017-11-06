2b27ef70b729fd9f697706cf11d5b229d36f1d2a
Nikhil Bose <nikbose97@gmail.com>
2017-11-06 11:59:29 -0800

    stash

---
+++ main.js
@@ -0,0 +1,7 @@
+firebaseRef = firebase.database().ref();
+
+
+var id = "Learn Salsa Dancing"
+var step1 = ["Book a Salsa Class","majestyinmotion.com/classes/","findsalsadancing.com/listing/la-jolla-marriott-la-jolla-ca/","https://www.groupon.com › ... › California › La Jolla › Things To Do › Fun & Leisure"]
+var step2 = ["Listen to more Salsa Music","www.apple.com/music","www.spotify.com","www.pandora.com"]
+var step3 = ["Buy Salsa dancing shoes","www.amazon.com","https://www.dancewearsolutions.com/shop/shoes","www.zappos.com"]
